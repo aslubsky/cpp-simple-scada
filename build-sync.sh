9758151636c36bd20ca5922a62e59f0557fa638e
Alex Slubsky <aslubsky@gmail.com>
2014-07-12 12:01:12 +0300

    add sync

--- build-sync.sh
+++ build-sync.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
-rm -rf Build
-mkdir Build
-
 g++ -c  "./sync.cpp" -std=gnu++0x  -g -O0 -Wall  -o ./Build/sync.o -I. -I. -I/usr/local/include -I/usr/include/mysql -I/usr/include
 g++ -o ./Build/sync ./Build/sync.o -L. -L/usr/local/lib -L/usr/lib/mysql -L/usr/lib -lmysqlclient -lconfig
